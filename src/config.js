c0a2d723ebd93c9a1638d362024ee23b4a174d9e
Joe Dickey <joseph.r.dickey@gmail.com>
2021-01-13 18:02:53 -0600

    push to deploy

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     ClIENT_ORIGIN: 'http://localhost:3000',
     API_TOKEN: process.env.API_TOKEN,
-    DATABASE_URL: process.env.DATABASE_URL || "postgresql://joe_dickey@localhost/casualist",
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://joe_dickey@localhost/casualist_test",
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://joe_dickey@localhost/casualist_test" ,
   }
