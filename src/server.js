f34d0ac918f7d7c29004d95db4ec8ea47cd0861d
Joe Dickey <joseph.r.dickey@gmail.com>
2021-01-14 11:23:13 -0600

    ssl

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const { PORT, DATABASE_URL} = require('./config')
 
 const db = knex({
     client: 'pg',
-    connection: `${DATABASE_URL}?rejectUnauthorized=true`,
+    connection: `${DATABASE_URL}?rejectUnauthorized=false`,
   })
   
   app.set('db', db)
