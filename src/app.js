6b2db5dfeb597416aaf0931b3b57527ebe12feb3
Joe Dickey <joseph.r.dickey@gmail.com>
2021-01-14 09:57:42 -0600

    debug

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use('/api/items', itemsRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
-    if (NODE_ENV === 'production') {
+    if (NODE_ENV !== 'production') {
         response = { error: { message: 'server error' } }
     } else {
         console.error(error)
